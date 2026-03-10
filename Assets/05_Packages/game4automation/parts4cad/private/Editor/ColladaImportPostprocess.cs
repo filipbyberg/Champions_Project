83ea86ee40b3ff4b860dbd47c8d5eab9c5587013
Filip Byberg <filipbyberg@gmail.com>
2026-03-10 22:25:55 +0100

    Test Setup

---
+++ Assets/05_Packages/game4automation/parts4cad/private/Editor/ColladaImportPostprocess.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ColladaImportPostprocess : AssetPostprocessor
+{
+
+    public void OnPreprocessModel()
+    {
+
+        Parts4CadSettings settings = (Parts4CadSettings) AssetDatabase.LoadAssetAtPath<ScriptableObject>("Assets/game4automation/Parts4Cad/Parts4CadSettings.asset");
+
+        if (settings != null)
+        {
+            ModelImporter modelImporter = assetImporter as ModelImporter;
+            if (assetPath.Contains("parts4cad"))
+            {
+                modelImporter.globalScale = settings.ImportScale;
+                modelImporter.addCollider = false;
+                modelImporter.preserveHierarchy = true;
+                Debug.Log("parts4cad Import Posprocessor");
+            }
+        }
+    }
+}
